623bbc513369e2ac943d864a4ca689ed71f9fcd2
Veeramani M <veeramanim2808@gmail.com>
2024-10-10 15:17:26 +0530

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,215 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private String currentInput = "";
+    private double firstNumber = 0;
+    private String operator = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultTextView);
+
+        setUpNumberButtons();
+        setUpOperatorButtons();
+        setUpEqualsButton();
+        setUpClearButton();
+    }
+    private void clearInput() {
+        currentInput = "";
+        firstNumber = 0;
+        operator = "";
+        resultTextView.setText("0");
+    }
+
+
+    private void setUpClearButton() {
+        Button clearButton = findViewById(R.id.buttonClear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearInput();
+            }
+        });
+    }
+
+
+    private void setUpNumberButtons() {
+        Button button0 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("0");
+            }
+        });
+        Button button1 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("1");
+            }
+        });
+        Button button2 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("2");
+            }
+        });
+        Button button3 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("3");
+            }
+        });
+        Button button4 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("4");
+            }
+        });
+        Button button5 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("5");
+            }
+        });
+        Button button6 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("6");
+            }
+        });
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("7");
+            }
+        });
+        Button button8 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("8");
+            }
+        });
+        Button button9 = findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appendNumber("9");
+            }
+        });
+
+
+    }
+
+    private void setUpOperatorButtons() {
+        Button addButton = findViewById(R.id.buttonAdd);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOperator("+");
+            }
+        });
+
+        Button subtractButton = findViewById(R.id.buttonSubtract);
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOperator("-");
+            }
+        });
+
+        Button multiplyButton = findViewById(R.id.buttonMultiply);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOperator("*");
+            }
+        });
+
+        Button divideButton = findViewById(R.id.buttonDivide);
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOperator("/");
+            }
+        });
+
+        // "=" Button to calculate the result
+        Button equalsButton = findViewById(R.id.buttonEquals);
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateResult();
+            }
+        });
+    }
+    private void setUpEqualsButton() {
+        Button equalsButton = findViewById(R.id.buttonEquals);
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateResult();
+            }
+        });
+    }
+
+
+    private void appendNumber(String number) {
+        currentInput += number;
+        resultTextView.setText(currentInput);
+    }
+
+    private void setOperator(String op) {
+        if (!currentInput.isEmpty()) {
+            firstNumber = Double.parseDouble(currentInput);
+            currentInput = "";
+            operator = op;
+        }
+    }
+
+
+    private void calculateResult() {
+        if (!currentInput.isEmpty()) {
+            double secondNumber = Double.parseDouble(currentInput);
+            double result = 0;
+
+            switch (operator) {
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+
+            resultTextView.setText(String.valueOf(result));
+            currentInput = String.valueOf(result);
+            operator = "";
+        }
+    }
+}
